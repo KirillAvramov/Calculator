e991ab71a9bd83e086f6577e59332dbbdccda71a
KirillAvramov <woks1234@gmail.com>
2016-12-23 14:09:27 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,45 +1,78 @@
-#include "stack.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include "bnumber.h"
-#define FIRST_SIZE 64
+#include "stack.h"
 
-static big_int ** stack;
-static int size;
-static int pointer;
-
-void stack_init() {
-    size = FIRST_SIZE;
-    stack = (big_int**)malloc(sizeof(big_int*)*size);
-    pointer = 0;
+void err_m() {
+    fprintf(stderr,"empty stack");
 }
 
-void stack_resize() {
-    size *=2;
-    stack = (big_int**)realloc(stack, sizeof(big_int*)*size);
-}
-
-void delete_stack() {
-    while((pointer-1)&&stack[pointer-1]) {
-        delete(stack[pointer-1]);
-        pointer--;
+int main()
+{
+    big_int *a;
+    big_int *b;
+    char c;
+    stack_init();
+    while(1) {
+        c=getchar();
+        switch(c) {
+        case ' ':
+        case '\n':
+            continue;
+        case '=':
+            print(stack_peek());
+            break;
+        case EOF:
+        case 'q':
+            delete_stack();
+            return 0;
+            break;
+        case '+':
+            if(stack_size()<2)
+            {
+                err_m();
+                break;
+            }
+            b = stack_pop();
+            a = stack_pop();
+            add(a,b);
+            stack_push(a);
+            break;
+        case '-':
+            if(stack_size()<2)
+            {
+                err_m();
+                break;
+            }
+            b = stack_pop();
+            a = stack_pop();
+            diff(a,b);
+            stack_push(a);
+            break;
+        case '*':
+            if(stack_size()<2)
+            {
+                err_m();
+                break;
+            }
+            b = stack_pop();
+            a = stack_pop();
+            stack_push(multiply(a,b));
+            break;
+        case '/':
+            if(stack_size()<2)
+            {
+                err_m();
+                break;
+            }
+            b = stack_pop();
+            a = stack_pop();
+            stack_push(divn(a,b));
+            break;
+        default:
+            a=scan_big_int((c=='-'?'-':0),(c=='-'?0:c));
+            stack_push(a);
+        }
     }
-    free(stack);
-}
-
-void stack_push(big_int * elem) {
-    if(!(size - pointer)) {
-        stack_resize();
-    }
-    stack[pointer++] = elem;
-}
-
-big_int * stack_peek() {
-    return stack[pointer-1];
-}
-
-big_int * stack_pop() {
-    return stack[--pointer];
-}
-
-int stack_size() {
-    return pointer;
+    return 0;
 }
